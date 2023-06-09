5e22e98ea72b037c6d7c21831dbbf7f238a02ddd
Herick75 <100052467+Herick75@users.noreply.github.com>
2023-06-09 17:33:41 -0300

    Update chroot.sh

--- Scripts/0.0.3/Init-En/chroot.sh
+++ Scripts/0.0.3/Init-En/chroot.sh
@@ -6,7 +6,7 @@ export USER=root
 export HOME=/root
 export LANGUAGE=C
 export LANG=C
-folder=/data/data/com.termux/files/home/Box4Droid/ubuntu-fs
+folder=/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu
 
 if [ -e $folder/sdcard ]; then
 echo ""
@@ -29,7 +29,7 @@ echo " Mounting /sdcard ... "
 sudo mount --bind /storage/emulated/0/ $folder/sdcard
 echo -e "\033[0;92m Mounting Done ! \033[0m"
 
-touch /data/data/com.termux/files/home/Box4Droid/ubuntu-fs/opt/chroot
+touch /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/opt/chroot
 
 echo -e "\033[0;92m Chrooting ... \033[0m"
 sudo chroot $folder /bin/su - root
